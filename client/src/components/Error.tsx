a98e1f76bc3582a8d30ba81c7c4d1969b6ae21b3
Zeyad <zezoayman0100@yahoo.com>
2025-07-15 05:49:29 +0300

    add return to home link in error component

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,4 +1,5 @@
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline"
+import { Link } from "react-router-dom";
 
 interface IProps {
     title: string;
@@ -10,7 +11,10 @@ const Error = ({ title, text }: IProps) => {
         <div className="grid h-full place-items-center place-content-center">
             <ExclamationTriangleIcon width={64} color="red" />
             <h2 className="mt-8 mb-5 text-2xl font-bold">{title}</h2>
-            <p>{text}</p>
+            <p className="mb-5">{text}</p>
+            <Link to={"/"} className="px-6 py-3 rounded-md bg-primary text-white font-semibold">
+                Return to Home
+            </Link>
         </div>
     )
 }
